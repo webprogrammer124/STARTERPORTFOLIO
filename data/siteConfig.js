feda8135328c8b0cc7e1d71154cb76a2c84f678d
Your Name <you@example.com>
2021-02-02 12:52:00 +0500

    done

---
+++ data/siteConfig.js
@@ -0,0 +1,122 @@
+module.exports = {
+  siteTitle: 'Hi! I\'m SeeMa YouSUf ZaI!',
+  siteDescription: `Create your online curriculum in just a few minutes with this starter`,
+  keyWords: ['gatsbyjs', 'react', 'curriculum'],
+  authorName: 'SeeMa YouSuF ZaI',
+  twitterUsername: '_franciscodf',
+  githubUsername: 'santosfrancisco',
+  authorAvatar: '/images/avatar.jpeg',
+  authorDescription: `Developer, passionate about what I do. Always interested in how the sites were made, I started to study HTML by hobby. <br />
+    In 2020 I start course in Saylani under the great teacher Miss Faiza Aziz
+    In 2020, I started to study Software Engineering at Virtual University at Karachi. <br />
+    I currently work as a frontend developer and mainly work with <strong>Javascript, NodeJS e React.</strong>`,
+  skills: [
+    {
+      name: 'HTML',
+      level: 70
+    },
+    {
+      name: 'CSS',
+      level: 60
+    },
+    {
+      name: 'Javascript',
+      level: 50
+    },
+    {
+      name: 'NodeJs',
+      level: 40
+    },
+    {
+      name: 'React',
+      level: 60
+    },
+    {
+      name: 'Git',
+      level: 70
+    },
+    /* more skills here */
+  ],
+  jobs: [
+    /* more jobs here */
+    {
+      school: "global",
+      begin: {
+        month: 'aug',
+        year: '2017'
+      },
+      duration: null,
+      occupation: "Global Academy School",
+      description: "I am part of the school,that school give me a world class education ."
+  
+    },  {
+      college: "Govt Girls Degree College ",
+      begin: {
+        month: 'sep',
+        year: '2017'
+      },
+      duration: null,
+      occupation: "Govt Girls Degree College",
+      description: "It a good college for girls."
+  
+    }, {
+      institute: "Saylani Mass I.T training",
+      begin: {
+        month: 'feb',
+        year: '2020'
+      },
+      duration: '1 yr e 5 mos',
+      occupation: "S.M.I.T",
+      description: "Saylani Mass IT Training Program. To Train 10 Thousand Developers in Pakistan. Living in an era driven by information technology makes investing in our youth ..."
+    }, {
+      University: "Virtual University",
+      begin: {
+        month: 'oct',
+        year: '2020'
+      },
+      duration: '4 yrs e 3 mos',
+      occupation: "Virtual University",
+      description: "Great University, Great students. Students learn excellent and new knowledge from Best teachers & tortures with affordable fee at their their doorstep. Students of Virtual University learn Effective Time management and the All staff of University is very cooperative & supportive."
+  
+    },
+  ],
+  portifolio: [
+    {
+      image: "/images/gatsby-starter-cv.png",
+      description: "Gatsby starter CV template",
+      url: "https://www.gatsbyjs.org/starters/santosfrancisco/gatsby-starter-cv/"
+    },
+    {
+      image: "/images/awesome-grid.png",
+      description: "Responsive grid for ReactJS",
+      url: "https://github.com/santosfrancisco/react-awesome-styled-grid"
+    },
+    /* more portifolio items here */
+  ],
+  social: {
+    twitter: "#",
+    linkedin: "#",
+    github: "https://github.com/webprogrammer124",
+    email: "seemagreat.911@gamil.com"
+  },
+  siteUrl: 'https://santosfrancisco.github.io/gatsbystarter-cv',
+  pathPrefix: '/gatsby-starter-cv', // Note: it must *not* have a trailing slash.
+  siteCover: '/images/cover.jpeg',
+  googleAnalyticsId: 'UA-000000000-1',
+  background_color: '#ffffff',
+  theme_color: '#25303B',
+  fontColor: "#000000cc",
+  enableDarkmode: true, // If true, enables dark mode switch
+  display: 'minimal-ui',
+  icon: 'src/assets/gatsby-icon.png',
+  headerLinks: [
+    {
+      label: 'Home',
+      url: '/',
+    },
+    {
+      label: 'Portifolio',
+      url: '/portifolio',
+    }
+  ]
+}
